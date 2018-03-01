43db76808bf0d0ebc1142f5ad4443b29c598a5ac
patrick <pmcdowell@okta.com>
2018-02-28 21:17:47 -0500

    update

---
+++ run.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    //http.ListenAndServe(":8080", nil)
+	http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil)
+
+}
